e8ea4ad514856872a6715c49c6d699b8514f33cc
xinghansun <xinghansun@gmail.com>
2026-02-04 15:39:52 -0500

    fixed wrong myeloid cluster selection; added singleR to assist with selection; added more detailed classification of ap/sup combination; added high ap high sup cells proportion test

--- scripts/03_mm_state_signature.R
+++ scripts/03_mm_state_signature.R
@@ -8,6 +8,9 @@ suppressPackageStartupMessages({
   library(ggplot2)
 })
 
+myeloid_labels <- c("Monocyte", "Macrophage", "DC", "Neutrophils")
+
+
 # Define gene sets for state scoring
 AP_GENES_REF <- c(
   "CIITA",
@@ -20,7 +23,6 @@ AP_GENES_REF <- c(
   "CTSS"
 )
 
-#SUP_GENES_REF <- c("CD274", "PDCD1LG2", "IDO1", "LGALS9", "LAIR1", "HAVCR2")
 SUP_GENES_REF <- c(
   # checkpoint ligands / inhibitory receptors (myeloid side)
   "CD274","PDCD1LG2","VSIR","LILRB1","LILRB2",
@@ -80,6 +82,19 @@ UMAPPlot(combined, label = TRUE)
 
 
 # Extract myeloid cells
+library(SingleR)
+library(celldex)
+ref <- HumanPrimaryCellAtlasData() 
+pred <- SingleR(test = as.SingleCellExperiment(combined), 
+                ref = ref, labels = ref$label.main)
+combined$SingleR_labels <- pred$labels
+
+DimPlot(combined, group.by = "SingleR_labels") # based on SingleR annotation
+tmp <- combined@meta.data
+table(tmp[tmp$SingleR_labels %in% myeloid_labels, "seurat_clusters"], 
+      tmp[tmp$SingleR_labels %in% myeloid_labels,"SingleR_labels"])
+
+
 myeloid_markers <- c("CD68", "CD14", "FCGR3A", 
                      "LYZ", "S100A8", "S100A9", 
                      "CSF1R", "ITGAX")
@@ -87,11 +102,11 @@ combined <- AddModuleScore(
   object = combined,
   features = list(myeloid_markers),
   name = "Myeloid_Score"
-)
+) # based on Myeloid Score 
 VlnPlot(combined, features = "Myeloid_Score1", group.by = "seurat_clusters")
 FeaturePlot(combined, features = "Myeloid_Score1")
 
-myeloid_obj <- subset(combined, idents = c(1, 5, 8))
+myeloid_obj <- subset(combined, idents = c(2, 9, 11))
 myeloid_obj$disease_stage <- case_when(
   grepl("NBM", myeloid_obj$orig.ident) ~ "Normal",
   grepl("MGUS", myeloid_obj$orig.ident) ~ "MGUS",
@@ -116,7 +131,10 @@ myeloid_obj <- FindNeighbors(myeloid_obj, reduction = "harmony", dims = 1:30)
 myeloid_obj <- FindClusters(myeloid_obj, resolution = 0.4)
 
 DimPlot(myeloid_obj, group.by = "orig.ident")
+DimPlot(myeloid_obj, group.by = "disease_stage")
+
 UMAPPlot(myeloid_obj, label = TRUE)
+
 
 # Calculate module scores for antigen presentation and immunosuppressive signatures
 AP_GENES  <- intersect(AP_GENES_REF, rownames(combined))
@@ -126,14 +144,14 @@ message(sprintf("Using %d antigen presentation genes: %s;\n\nDropped %d genes no
                 length(AP_GENES), 
                 paste(AP_GENES, collapse = ", "), 
                 length(AP_GENES_REF) - length(AP_GENES)
-                )
-        )
+)
+)
 message(sprintf("Using %d immunosuppressive genes: %s;\n\nDropped %d genes not found in dataset.", 
                 length(SUP_GENES), 
                 paste(SUP_GENES, collapse = ", "), 
                 length(SUP_GENES_REF) - length(SUP_GENES)
-                )
-        )
+)
+)
 
 define_myeloid_states <- function(obj, ap_genes, sup_genes) {
   obj <- AddModuleScore(obj, features = list(ap_genes), name = "AP_Score")
@@ -141,14 +159,21 @@ define_myeloid_states <- function(obj, ap_genes, sup_genes) {
   obj$AP_val <- obj$AP_Score1
   obj$SUP_val <- obj$SUP_Score1
   
-  # only use normal myeloid cells to define thresholds
-  norm_cells <- which(obj$disease_stage == "Normal")
-  ap_thr  <- median(obj$AP_val[norm_cells], na.rm = TRUE)
-  sup_thr <- median(obj$SUP_val[norm_cells], na.rm = TRUE)
+  ap_thr  <- median(obj$AP_val, na.rm = TRUE)
+  sup_thr <- median(obj$SUP_val, na.rm = TRUE)
   
   obj$functional_state <- "Intermediate"
-  obj$functional_state[obj$AP_val <= ap_thr & obj$SUP_val >= sup_thr] <- "Suppressive_like"
-  obj$functional_state[obj$AP_val > ap_thr & obj$SUP_val < sup_thr] <- "Activated_like"
+  obj$functional_state[obj$AP_val <= ap_thr & obj$SUP_val >= sup_thr] <- "AP_low_Sup_high"
+  obj$functional_state[obj$AP_val > ap_thr & obj$SUP_val < sup_thr] <- "AP_high_Sup_low"
+  obj$functional_state[obj$AP_val <= ap_thr & obj$SUP_val < sup_thr] <- "AP_low_Sup_low"
+  obj$functional_state[obj$AP_val > ap_thr & obj$SUP_val >= sup_thr] <- "AP_high_Sup_high"
+  
+  obj$functional_state <- factor(obj$functional_state, 
+                                     levels = c("AP_high_Sup_high",
+                                                "AP_high_Sup_low", 
+                                                "Intermediate",
+                                                "AP_low_Sup_high",
+                                                "AP_low_Sup_low"))
   
   return(obj)
 }
@@ -164,9 +189,52 @@ FeaturePlot(myeloid_obj,
 VlnPlot(myeloid_obj, features = c("AP_val", "SUP_val"), pt.size = 0)
 
 DimPlot(myeloid_obj, 
+        group.by = "functional_state")
+
+DimPlot(myeloid_obj, 
         group.by = "functional_state", 
-        cols = c("red", "blue", "grey"))
+        split.by = "disease_stage")
 
 
+# Check AP high SUP high distribution across disease stages
+# A. pooled distribution
+prop_data <- myeloid_obj@meta.data %>%
+  group_by(disease_stage, functional_state) %>%
+  summarise(count = n()) %>%
+  mutate(percentage = count / sum(count) * 100)
 
+prop_data$disease_stage <- factor(prop_data$disease_stage, 
+                                  levels = c("Normal", "MGUS", "SMM", "MM"))
 
+ggplot(prop_data, aes(x = disease_stage, y = percentage, fill = functional_state)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.7, color = "white") +
+  scale_fill_manual(values = c("AP_high_Sup_high" = "#E41A1C",
+                               "AP_high_Sup_low"  = "#4DAF4A",
+                               "AP_low_Sup_high"  = "#377EB8",
+                               "AP_low_Sup_low"   = "#984EA3")) +
+  theme_classic() +
+  labs(title = "Proportion of Functional States across Stages",
+       x = "Disease Stage", y = "Proportion (%)", fill = "Functional State") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# B. 
+sample_props <- myeloid_obj@meta.data %>%
+  group_by(orig.ident, disease_stage, functional_state) %>%
+  summarise(count = n()) %>%
+  mutate(prop = count / sum(count)) %>%
+  ungroup()
+
+library(ggpubr)
+
+ggplot(filter(sample_props, functional_state == "AP_high_Sup_high"), 
+       aes(x = disease_stage, y = prop, fill = disease_stage)) +
+  geom_boxplot(outlier.shape = NA)  + 
+  geom_jitter(width = 0.2, alpha = 0.6) +
+  stat_compare_means(method = "wilcox.test", 
+                     comparisons = list(c("Normal", "MM"), 
+                                        c("Normal", "SMM"),
+                                        c("Normal", "MGUS")
+                                        )) +
+  theme_classic() +
+  labs(title = "Proportion of AP_high-SUP_high per Sample", 
+       y = "Proportion")
