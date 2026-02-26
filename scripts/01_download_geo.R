3fdafc640303e29e50622017999a4489a8fca14d
xinghansun <xinghansun@gmail.com>
2026-02-25 20:40:43 -0500

    now also downloads MMRF-COMMPASS dataset

--- scripts/01_download_geo.R
+++ scripts/01_download_geo.R
@@ -1,5 +1,6 @@
 suppressPackageStartupMessages({
   library(GEOquery)
+  library(TCGAbiolinks)
 })
 
 dir.create("../data/geo", recursive = TRUE, showWarnings = FALSE)
@@ -30,7 +31,16 @@ download_geo_matrix <- function(gse, outdir) {
 download_geo_supp("GSE124310", "../data/geo/GSE124310") # discovery scRNA
 download_geo_supp("GSE163278", "../data/geo/GSE163278") # validation scRNA
 
-# bulk
+# bulk mircroarray
 download_geo_matrix("GSE136337", "../data/geo/GSE136337") # validation bulk RNA
+
+# bulk RNA-seq
+query_exp <- GDCquery(
+  project = "MMRF-COMMPASS",
+  data.category = "Transcriptome Profiling",
+  data.type = "Gene Expression Quantification",
+  workflow.type = "STAR - Counts" 
+)
+GDCdownload(query_exp, directory = "../data/MMRF_COMMPASS", method = "api", files.per.chunk = 10) 
 
 message("All downloads finished.")
